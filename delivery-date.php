938bc3599807a8bbe593829915f72147b9c42d0e
FABRIZIO DEL TUFO <fabrizio.deltufo@gmail.com>
2024-01-12 19:15:56 +0100

    new plugin header

--- delivery-date.php
+++ delivery-date.php
@@ -1,19 +1,18 @@
 <?php
 /*
 * Plugin Name:       Delivery Date
-* Plugin URI:        https://github.com/fabriziodeltufo/delivery-date
-* Description:       Show Delivery Date 
+* Plugin URI:        https://wewebby.gumroad.com/
+* Description:       Manage / show a Delivery Date for a woocommerce product.
 * Version:           1.0.0
 * Requires at least: 6.4.2
 * Requires PHP:      7.4.33
-* Author:            Fabrizio Del Tufo
-* Author URI:        https://github.com/fabriziodeltufo
+* Author:            WeWebby.com
+* Author URI:        https://wewebby.com
 * License:           GPL v2 or later
 * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 * Text Domain:       fdtdd
 * Domain Path:       /languages
 */
-
 
 
 /* SECURITY  */
